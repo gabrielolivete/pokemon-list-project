bd650dfed3e86bb5a4f304cc0515caf678ae2dc4
Gabriel Olivete <115937639+gabrielolivete@users.noreply.github.com>
2023-05-15 15:38:37 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const botaoAlterartema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterartema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if(modoEscuroEstaAtivo) {
+      imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+      }
+});
